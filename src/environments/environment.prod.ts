c93c67dafb6f245cacea596854650d00d335f6cc
Maria Fernanda Montilva <montilvamafer88@gmail.com>
2026-08-10 22:55:07 +0200

    chore: configurar build de produccion y despliegue en Netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://TU-BACKEND.com',
-  supabaseUrl: '',
-  supabaseAnonKey: '',
-  storageBucket: 'animals',
-  storagePublicUrl: ''
+  apiUrl: 'PENDIENTE_URL_RENDER',
+  supabaseUrl: 'https://qjxrfmmwukafovfzdjlq.supabase.co',
+  supabaseAnonKey: 'sb_publishable_Q8IZ8JC8cKm6Ns_uDquvwA_e12Ls2c8',
+  storageBucket: 'media-assets',
+  storagePublicUrl: 'https://qjxrfmmwukafovfzdjlq.supabase.co/storage/v1/object/public'
 };
